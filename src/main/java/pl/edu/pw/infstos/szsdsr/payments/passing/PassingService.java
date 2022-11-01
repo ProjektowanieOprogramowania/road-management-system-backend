5eeeac4a05e40eab7a0023f136835202a5313dd5
Mateusz Mazewski <mateusz.mazewski.stud@pw.edu.pl>
2022-11-01 21:33:21 +0100

    Add remaining CRUD methods

--- src/main/java/pl/edu/pw/infstos/szsdsr/payments/passing/PassingService.java
+++ src/main/java/pl/edu/pw/infstos/szsdsr/payments/passing/PassingService.java
@@ -3,6 +3,8 @@ package pl.edu.pw.infstos.szsdsr.payments.passing;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 public class PassingService {
 
@@ -16,4 +18,26 @@ public class PassingService {
         passing.setId(null);
         return passingRepository.save(passing);
     }
+
+    public Optional<Passing> getPassing(Long id) {
+        return passingRepository.findById(id);
+    }
+
+    public Optional<Passing> updatePassing(Passing passing) {
+        if (passingRepository.existsById(passing.getId())) {
+            return Optional.of(passingRepository.save(passing));
+        } else {
+            return Optional.empty();
+        }
+    }
+
+    public boolean deletePassing(Long id) {
+        if (passingRepository.existsById(id)) {
+            passingRepository.deleteById(id);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
 }
--- src/main/java/pl/edu/pw/infstos/szsdsr/payments/penalty/PenaltyService.java
+++ src/main/java/pl/edu/pw/infstos/szsdsr/payments/penalty/PenaltyService.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class PenaltyService {
@@ -21,6 +22,27 @@ public class PenaltyService {
 
     public List<Penalty> getAllPenalties(Long userId) {
         return penaltyRepository.findAllByUserId(userId);
+    }
+
+    public Optional<Penalty> getPenalty(Long id) {
+        return penaltyRepository.findById(id);
+    }
+
+    public Optional<Penalty> updatePenalty(Penalty penalty) {
+        if (penaltyRepository.existsById(penalty.getId())) {
+            return Optional.of(penaltyRepository.save(penalty));
+        } else {
+            return Optional.empty();
+        }
+    }
+
+    public boolean deletePenalty(Long id) {
+        if (penaltyRepository.existsById(id)) {
+            penaltyRepository.deleteById(id);
+            return true;
+        } else {
+            return false;
+        }
     }
 
 }
