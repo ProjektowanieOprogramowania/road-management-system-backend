ed8e82770bba71448c1d5117a56415f6ffdb1cc2
folejniczak <filip.olejniczak@tecna.pl>
2022-11-03 09:36:36 +0100

    controllers corrected

--- src/main/java/pl/edu/pw/infstos/szsdsr/tolls/controllers/SubscriptionApiController.java
+++ src/main/java/pl/edu/pw/infstos/szsdsr/tolls/controllers/SubscriptionApiController.java
@@ -1,9 +1,11 @@
 package pl.edu.pw.infstos.szsdsr.tolls.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 import pl.edu.pw.infstos.szsdsr.generated.api.SubscriptionApi;
+import pl.edu.pw.infstos.szsdsr.generated.models.SubscriptionDTO;
 import pl.edu.pw.infstos.szsdsr.generated.models.SubscriptionInfoDTO;
 import pl.edu.pw.infstos.szsdsr.tolls.services.TollService;
 
@@ -20,6 +22,16 @@ public class SubscriptionApiController implements SubscriptionApi {
     public ResponseEntity<List<SubscriptionInfoDTO>> getSubscriptionInfo() {
 
         return (ResponseEntity<List<SubscriptionInfoDTO>>) tollService.getSubscriptionInfo();
+    }
+
+    @Override
+    public ResponseEntity purchaseSubsription(SubscriptionDTO subscriptionDTO) {
+        try{
+            tollService.purchaseSubscription(subscriptionDTO);
+            return new ResponseEntity(HttpStatus.OK);
+        } catch (IllegalArgumentException e) {
+            return new ResponseEntity(HttpStatus.NOT_ACCEPTABLE);
+        }
     }
 
 }
