0bb573fe692faa9d1a6967d0d3b22c778b6ba629
Tomasz Gardocki <tgardocki1@gmail.com>
2022-11-09 19:40:30 +0100

    Disable CORS

--- src/main/java/pl/edu/pw/infstos/szsdsr/AppConfiguration.java
+++ src/main/java/pl/edu/pw/infstos/szsdsr/AppConfiguration.java
@@ -5,15 +5,22 @@ import com.fasterxml.jackson.databind.json.JsonMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class AppConfiguration {
+public class AppConfiguration implements WebMvcConfigurer {
 
     @Bean
     public ObjectMapper objectMapper() {
         return JsonMapper.builder()
                 .addModule(new JavaTimeModule())
                 .build();
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedMethods("*");
     }
 
 }
